18bc583cb2ed37f86c660b54457cfd6153572c74
Andres Saa <andressaa.dev@gmail.com>
2020-08-17 16:32:10 -0500

    Modificando rutas de usuarios y sus métodos

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -12,6 +12,12 @@ use Symfony\Component\HttpFoundation\Response;
 class ApiController extends Controller
 {
     public $loginAfterSignUp = true;
+    protected $user;
+
+    public function __construct()
+    {
+        $this->user = JWTAuth::parseToken()->authenticate();
+    }
 
     public function register(RegisterAuthRequest $request)
     {
@@ -81,5 +87,103 @@ class ApiController extends Controller
         $user = JWTAuth::authenticate($request->token);
 
         return response()->json(['user' => $user]);
+    }
+
+    public function index()
+    {
+        return $this->user
+            ->users()
+            ->get(['id', 'name', 'email', 'phone_number', 'user_type', 'created_at'])
+            ->toArray();
+    }
+    public function show($id)
+    {
+        $user = $this->user->users()->find($id);
+
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, user with id ' . $id . ' cannot be found'
+            ], 400);
+        }
+
+        return $user;
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'phone_number' => 'required',
+            'user_type' => 'required',
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->phone_number = $request->phone_number;
+        $user->user_type = $request->user_type;
+
+        if ($this->user->users()->save($user))
+            return response()->json([
+                'success' => true,
+                'user' => $user
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, user could not be added'
+            ], 500);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = $this->user->users()->find($id);
+
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, user with id ' . $id . ' cannot be found'
+            ], 400);
+        }
+
+        $updated = $user->fill($request->all())
+            ->save();
+
+        if ($updated) {
+            return response()->json([
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, user could not be updated'
+            ], 500);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $user = $this->user->users()->find($id);
+
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, user with id ' . $id . ' cannot be found'
+            ], 400);
+        }
+
+        if ($user->delete()) {
+            return response()->json([
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'user could not be deleted'
+            ], 500);
+        }
     }
 }
